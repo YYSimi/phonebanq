ef49391ab6239c2c3f9c9085f9dac8f0f7eafbc3
Daniel Stiner <danstiner@gmail.com>
2016-11-23 16:55:00 -0800

    Move data fetching into myTasks component
    Keeping it in the top level makes lower components reusable

--- imports/ui/tasks/myTasks.js
+++ imports/ui/tasks/myTasks.js
@@ -5,12 +5,6 @@ import { Meteor } from 'meteor/meteor';
 import './myTasks.html'
 import '../../api/tasks.js'
 
-// TODO:  Put this in a central location.
-var taskTypes = ["dailyCallPrompts", "weeklyCallPrompts"];
-var taskCollections = {
-    dailyCallPrompts : DailyCallPrompts,
-    weeklyCallPrompts : WeeklyCallPrompts
-}
 
 // TODO:  Is this the right place to do the subscription?
 Template.myTasks.onCreated(function () {
@@ -18,27 +12,22 @@ Template.myTasks.onCreated(function () {
     Meteor.subscribe('dailyCallPrompts');
     Meteor.subscribe('weeklyCallPrompts');
 });
-
 Template.myTasks.helpers({
     getTasks() {
         var tasks = UserTasks.find({ user_id: Meteor.userId() });
         console.log("Found " + tasks.count() + " tasks for uid" + Meteor.userId());
-        return tasks;
-    }
-})
-
-Template.Task.helpers({
-    taskInfo() { 
-        console.log(this.task);
         
-        //TODO:  Make this OO-ified
-        switch (this.task.task_type) {
-            case "dailyCallPrompts":
-                console.log(taskCollections[this.task.task_type]);
-                return DailyCallPrompts.findOne(new Mongo.ObjectID(this.task.task_id)).supporter_script;
-            case "weeklyCallPrompts":
-                console.log(taskCollections[this.task.task_type]);
-                return taskCollections[this.task.task_type].findOne(new Mongo.ObjectID(this.task.task_id)).Script;
-        }
+        return tasks.map(task => {
+            switch (task.task_type) {
+                case "dailyCallPrompts":
+                    return {
+                        info: DailyCallPrompts.findOne(new Mongo.ObjectID(task.task_id)).supporter_script
+                    };
+                case "weeklyCallPrompts":
+                    return {
+                        info: WeeklyCallPrompts.findOne(new Mongo.ObjectID(task.task_id)).Script
+                    };
+            }
+        });
     }
 })
