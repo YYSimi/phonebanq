f049699391350aaf977e6e09ab5bd62d23e01fcd
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2016-11-24 19:20:57 -0800

    tasks now update active status, old tasks get purged after 3 days

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -46,10 +46,10 @@ function CreateRandomUserTask(userId) {
     var fFoundTask = false;
     var foundTaskType;
     var foundTask;
-    var nMaxTasks = 10; //Todo:  put this in settings.
+
     
     Meteor.call('users.updateTaskCount');  //TODO:  Figure out when/where this should be updated.
-    if (Meteor.call('users.getTaskCount') >= nMaxTasks) {
+    if (Meteor.call('users.getTaskCount') >= Meteor.settings.public.tasks.nMaxActiveTasks) {
         return false;
     }
     
@@ -79,17 +79,20 @@ function CreateRandomUserTask(userId) {
 
     if (fFoundTask) {
         console.log(foundTask);
-        var today = new Date().setUTCHours(0,0,0,0);
-        var tomorrow = new Date().setUTCHours(23, 59, 59, 999);
-        // TODO:  Figure out security model here!  MUST DO THIS BEFORE PUBLIC PUBLISH.
+        var today = new Date();
+        today.setUTCHours(0,0,0,0);   // TODO:  Consider moving off of UTC?
+        var tomorrow = new Date(today);
+        tomorrow.setUTCHours(23, 59, 59, 999)
         var userTask = {
             user_id: userId,
             task_type: foundTaskType,
             task_id: foundTask._id._str,
             task_statistics: "TBA",
-            given_on: tomorrow,
+            given_on: today,
             lasts_until: tomorrow,
+            is_active : true,
             is_completed: false,
+            is_repeatable: false,
             never_show_again: false
         }
         console.log(userTask);
@@ -99,6 +102,30 @@ function CreateRandomUserTask(userId) {
     }
 };
 
+function UpdateUserTasks(userId) {
+    // Mark any expired active tasks as inactive
+    var expiredTaskCutoffDate = new Date();
+    activeExpired = UserTasks.find({
+        user_id: userId,
+        is_active: true,
+        lasts_until : { $lt : new Date() }
+    });
+
+    activeExpired.forEach( function (item) {
+        item.is_active = false;
+    });
+
+    // Purge old tasks that are non-active, non-completed, and non-"never show again"
+    var oldTaskCutoffDate = new Date();
+    oldTaskCutoffDate.setDate(oldTaskCutoffDate.getDay() - Meteor.settings.public.tasks.nTaskRetryDelayDays);
+    UserTasks.remove({
+        user_id: userId,
+        is_completed: false,
+        is_repeatable: false,
+        never_show_again: false,
+        lasts_until : { $lt : oldTaskCutoffDate } } );
+}
+
 Meteor.methods({
     // Create a new task for the given user
     'tasks.createRandom'() {
@@ -106,7 +133,8 @@ Meteor.methods({
             throw new Meteor.Error('not-autherized')
         }
         if (Meteor.isServer) {
-            CreateRandomUserTask(Meteor.userId());
+            UpdateUserTasks(Meteor.userId());
+            //CreateRandomUserTask(Meteor.userId());
         }
     }
 })
