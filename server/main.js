59f6ce6cfe61380aab6b94cdb9b32ae1ac7dc6d9
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2016-11-30 22:17:54 -0800

    Facebook Notifications now work!

--- server/main.js
+++ server/main.js
@@ -1,8 +1,15 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
 
 import '../imports/api/users.js';
 import '../imports/api/tasks.js';
-import { PopulateUserTasks, UpdateUserTasks } from '../lib/common.js'
+import { PopulateUserTasks, UpdateUserTasks } from '../lib/common.js';
+
+// TODO:  Figure out how we are supposed to abstract things nicely in JS.
+// server-side global variables go here for now.
+var appInfo = {
+    fbAppAccessToken: undefined
+}
 
 Meteor.startup(() => {
     Houston.add_collection(Meteor.users);
@@ -12,10 +19,33 @@ Meteor.startup(() => {
         {
             $set : {
                 "appId": process.env['FACEBOOK_APP_ID'],
-                "secret": process.env['FACEBOOK_SECRET']
+                "secret": process.env['FACEBOOK_SECRET'],
             }
         }
     )
+
+    httpRequestStr='https://graph.facebook.com/oauth/access_token' +
+            "?client_id=" + process.env['FACEBOOK_APP_ID'] +
+            "&client_secret=" + process.env['FACEBOOK_SECRET'] +
+            "&grant_type=client_credentials";
+
+    console.log("Request string is " + httpRequestStr);
+
+    HTTP.get(httpRequestStr, 
+        function(error, response) { 
+            if (error) {
+                console.log( error );
+            }
+            else {
+                console.log( response );
+                console.log("setting token to " + response.content.split('=')[1])
+                appInfo.fbAppAccessToken = response.content.split('=')[1];
+                console.log("set token to " + appInfo.fbAppAccessToken)
+            }
+        }
+    )
+
+
     UpdateAllUserTasks();
     var fnSetUpdateTimer = function () {
         var midnight = new Date();
@@ -26,7 +56,7 @@ Meteor.startup(() => {
         var timeUntilMidnight = (midnight.getTime() - new Date().getTime())
         Meteor.setTimeout(
             function() { 
-                UpdateAllUserTasks(); 
+                UpdateAllUserTasks();
                 fnSetUpdateTimer() }, 
 //            timeUntilMidnight)
                 1000*10);
@@ -60,8 +90,8 @@ Accounts.onLogin(function(loginAttempt) {
     }
 
     // TODO:  Figure out when "profile" gets populated
-    // TODO:  Figure out best practices for querying javascript objects that may be null. 
-    if (!Meteor.user().profile || Meteor.user().profile.fHasLoggedInBefore) {
+    // TODO:  Figure out best practices for querying javascript objects that may be null.
+    if (!Meteor.user().profile || !Meteor.user().profile.fHasLoggedInBefore) {
         OnFirstLogin(Meteor.userId());
         Meteor.users.update(
             { _id: Meteor.userId() },
@@ -78,12 +108,41 @@ function UpdateAllUserTasks(){
     Meteor.users.find().forEach(function(user) {
         console.log("updating user tasks for " + user._id);
         UpdateUserTasks(user._id);
-        PopulateUserTasks(user._id);
+        var nNewTasksCreated = PopulateUserTasks(user._id);
+        if (nNewTasksCreated > 0) {
+            NotifyFacebookUser(user);
+        }
     })
 }
 
 function OnFirstLogin(userId) {
     console.log("running OnFirstLogin");
     UpdateUserTasks(userId);
-    PopulateUserTasks(userId);
+    var nNewTasksCreated = PopulateUserTasks(userId);
+    if (nNewTasksCreated > 0) {
+        NotifyFacebookUser(Meteor.User());
+    }
+}
+
+// TODO:  Make sure we've generated the app access token before sending this out.'
+function NotifyFacebookUser(user) {
+    console.log(user);
+    userFbInfo = user.services.facebook;
+    
+    httpRequestStr='https://graph.facebook.com/' +
+        userFbInfo.id +  '/notifications' +
+        "?access_token=" + appInfo.fbAppAccessToken +
+        "&href=/myTasks" +
+        "&template=New task available #America";
+    
+    console.log("Posting http request to " + httpRequestStr)
+
+    HTTP.post(httpRequestStr, {}, function (error, response) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log(response);
+        }
+    });
 }
