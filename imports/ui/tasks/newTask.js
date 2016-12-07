a7236bc5ee9257e344615bc45fbfa21131aeb784
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2016-12-07 09:07:55 -0800

    newTask page now has a preview button!

--- imports/ui/tasks/newTask.js
+++ imports/ui/tasks/newTask.js
@@ -7,6 +7,9 @@ import { Task, PhoneTask } from '../../api/taskClasses.js'
 
 import './newTask.html'
 
+// TODO: Is this really the best way to force an update on preview click? 
+var nPreviewClicks = new ReactiveVar(0);
+
 Template.authenticatedUserNewTask.onCreated(function () {
     Meteor.subscribe('userTasks');
     Meteor.subscribe('tasks');
@@ -55,5 +58,44 @@ Template.authenticatedUserNewTask.events({
 
         Meteor.call('tasks.registerNewTask', task, phoneTask);
         return false;
+    },
+
+    'click .js-newTask-preview'() {
+        console.log("nPreviewClicks is currently " + nPreviewClicks.get())
+        nPreviewClicks.set(nPreviewClicks.get()+1);
+        console.log("nPreviewClicks is now " + nPreviewClicks.get())
     }
 });
+
+Template.phoneTaskDetail.helpers({
+    fShouldRenderTaskPreview() {
+        return nPreviewClicks.get();
+    },
+    taskPreviewInfo() {
+        if (nPreviewClicks.get() != 0) {
+            return {
+                task: new Task(
+                    $("#tiny-description").val(),
+                    $("#brief-description").val(),
+                    new Date(), // TODO: handle start/end dates properly.
+                    new Date(),
+                    PBTaskTypesEnum.phone,
+                    null,
+                    5 //TODO:  pump priority into the form.
+                    ),
+                taskDetail: 
+                    // TODO:  Make this work for more than just phone taks.
+                    new PhoneTask(
+                        $("#general-script").val(),
+                        $("#supporter-script").val(),
+                        $("#opposition-script").val(),
+                        null,
+                        $("#call-my-national-senators").val(),
+                        $("#call-my-national-representatives").val(),
+                        null,
+                        null,
+                        null)
+            }
+        }
+    }
+})
