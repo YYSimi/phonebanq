c511647e9c9e02605d63d747c2b860307f28a0a4
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2016-12-11 12:16:29 -0800

    level up animations are now smooth!

--- imports/ui/userXpWidgets.js
+++ imports/ui/userXpWidgets.js
@@ -4,23 +4,24 @@ import './userXpWidgets.html';
 
 // TODO:  Move the leveling system into its own file.
 
-// TODO:  Make the animations smooth at level boundaries -- right now, level ups just drain your XP bar.  Bad UX!
-
 var levelXpReqs = [0, 1, 5, 10, 20, 50] //levelXpReqs[i] is the _Total_ xp required to reach level i.
 
+var displayedLevel = new ReactiveVar(-1);
+var fLevelUpAnimationPlaying = false;
+
 function getXpReqForNextLevel() {
-    currentLevel = getCurrentLevel();
+    var currentLevel = getCurrentLevel();
     return levelXpReqs[currentLevel+1] - levelXpReqs[currentLevel];
 }
 
 function getXpIntoCurrentLevel() {
-    currentLevel = getCurrentLevel();
+    var currentLevel = getCurrentLevel();
     return getCurrentXp() - levelXpReqs[currentLevel];
 }
 
 function getCurrentXp() {
-    retval = 0;
-    user = Meteor.user();
+    var retval = 0;
+    var user = Meteor.user();
     if (user && user.profile && user.profile.progression && user.profile.progression.xp) {
         userXp = user.profile.progression.xp
         if (userXp < 0) {userXp = 0;}
@@ -31,12 +32,29 @@ function getCurrentXp() {
 
 function getCurrentLevel() {
     var currentXp = getCurrentXp();
-    var i = -1;
+    var lastDisplayedLevel = displayedLevel.get();
+    var currentLevel = -1;
     levelXpReqs.forEach( (xpNeeded) => {
-        if (xpNeeded <= currentXp) {++i}
+        if (xpNeeded <= currentXp) {++currentLevel}
     })
+    // Haven't displayed any level yet, so just go to the current level.
+    if (lastDisplayedLevel == -1) {
+        displayedLevel.set(currentLevel);
+    }
+    // Don't immediately update the displayed level -- give the XP bar a chance to fill and animate.
+    else if (lastDisplayedLevel != currentLevel) {
+        var levelUpAnimationDuration = 1000;
+        if (!fLevelUpAnimationPlaying) {
+            fLevelUpAnimationPlaying = true;
+            setTimeout(function() {
+                displayedLevel.set(currentLevel);
+                fLevelUpAnimationPlaying = false;
+            },
+                levelUpAnimationDuration);
+        }
+    }
 
-    return i;
+    return displayedLevel.get();
 }
 
 Template.userXpBar.helpers({
