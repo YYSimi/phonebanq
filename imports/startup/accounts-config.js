902d5eb771114ade99bfb1fbbe2f0f835c9487b1
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2016-12-11 22:16:21 -0800

    Changes login box to require email rather than usernames.  Facebook now also asks for the email permission

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,8 +1,8 @@
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_ONLY',
+    passwordSignupFields: 'EMAIL_ONLY',
     requestPermissions: {
-        facebook: ['user_location']
+        facebook: ['user_location', 'email']
     }
 })
