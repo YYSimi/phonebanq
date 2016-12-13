b9f1cce62a9bfe8dfe8c55df4572a53e99314e18
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2016-12-12 21:23:15 -0800

    Splits cancel task into cancel and uncomplete, separating the two functionalities.  Canceling/uncompleting tasks no longer triggers a userTasks cursor update, getting rid of an unneccesary DB resubscribe when canceling/uncompleting

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -99,6 +99,7 @@ Meteor.methods({
         })
     },
 
+    // Stops a task from being given to any additional users.
     'tasks.disableTask'(taskId) {
         check(taskId, Mongo.ObjectID)
         var user = Meteor.user();
@@ -109,6 +110,7 @@ Meteor.methods({
         }
     },
 
+    // Completes a task and moves it off of the active task list
     'tasks.completeTask'(userTaskId) {
         check(userTaskId, Mongo.ObjectID); // TODO:  Figure out MongoId vs String relationship in collections.
         console.log('completing task ' + userTaskId );
@@ -145,7 +147,37 @@ Meteor.methods({
 
     },
 
+    // Removes a task from the active task list.
     'tasks.cancelTask'(userTaskId) {
+        check(userTaskId, Mongo.ObjectID); // TODO:  Figure out MongoId vs String relationship in collections.
+        console.log('cancelling task ' + userTaskId );
+        
+        var userTask = UserTasks.findOne(userTaskId);
+        var user = Meteor.user();
+        var userId = Meteor.userId();
+        
+        if (userId != userTask.user_id) {
+            throw new Meteor.Error('not-autherized', "The logged-in user does not own this task.");
+        }
+        
+        task = Tasks.findOne(new Mongo.ObjectID(userTask.task_id));
+        if (!task) {
+            throw new Meteor.Error('bad-state', "The associated task does not exist")
+        }
+
+        // TODO:  Make class-like interface that pairs the remove and update call together.
+        if (userTask.is_active) {
+            UserTasks.update(
+                { _id: userTaskId },
+                { $set: { "is_active" : false} }
+            );
+            Meteor.users.update(userId, {$inc: {"statistics.activeTasks": -1} });
+        }
+    },
+
+    // Moves a task back to the active task list and marks it as not completed.
+    // TODO:  Think through the details of this use case.  e.g. Do we change the expiry date? 
+    'tasks.unCompleteTask'(userTaskId) {
         check(userTaskId, Mongo.ObjectID); // TODO:  Figure out MongoId vs String relationship in collections.
         console.log('uncompleting task ' + userTaskId );
         
@@ -171,19 +203,22 @@ Meteor.methods({
             var taskXp = task.xp_value || 1;    //TODO:  Make a file that stores default values for DB not-fully-initialized DB elements.
             var newXp = currentXp - taskXp;
             Meteor.users.update(userId, {$set : {"profile.progression.xp" : newXp}})
-        }
 
-        // TODO:  Make class-like interface that pairs the remove and update call together.
-        UserTasks.remove(userTaskId);
+           if (!userTask.is_active) {
+                UserTasks.update(
+                    { _id: userTaskId },
+                    { $set: { "is_active" : true, "is_completed": false} }
+                );
+                Meteor.users.update(userId, {$inc: {"statistics.activeTasks": 1} });
+            }
 
-        if (userTask.is_active) {
-            Meteor.users.update(userId, {$inc: {"statistics.activeTasks": -1} });
         }
 
     },
 
     // TODO:  Create a class-like interface for managing UserTasks, then have the Meteor methods call directly into the class.
 
+    // This user will never again see the given task.
     'tasks.hideTaskForever'(userTaskId) {
         check(userTaskId, Mongo.ObjectID); // TODO:  Be more specific about the kind of object.  Figure out MongoId vs String relationship in collections.
         userTask = UserTasks.findOne(userTaskId);
@@ -195,7 +230,7 @@ Meteor.methods({
         // TODO:  Really, create a class-like interface to pair these two calls together.
         UserTasks.update(
             { _id: userTaskId },
-            { $set: { "is_active" : "false",  "never_show_again" : true} }
+            { $set: { "is_active" : false,  "never_show_again" : true} }
         );
 
         if (userTask.is_active) {
