c684e514f2e9932200750ecc78f5c2290ff33ae6
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2017-01-01 14:43:08 -0500

    Adds test framework to project, first unit test written!

---
+++ server/main.tests.js
@@ -0,0 +1,25 @@
+import './main.js'
+import { Meteor } from 'meteor/meteor';
+import { resetDatabase } from 'meteor/xolvio:cleaner';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+if (Meteor.isServer) {
+    describe('user creation', () => {
+        beforeEach(() => {
+            resetDatabase()
+        })
+
+        it('usernames are properly generated for e-mail based logins', () => {
+            var email = 'test_account@test.com'
+            var password = '123456'
+            console.log(Meteor.users.find().fetch());
+            var userId = Accounts.createUser({email: email, password : password});
+
+            var user = Meteor.users.findOne(userId)
+            assert(user, "failed to find user");
+            console.log(user);
+            assert.equal(user.username, 'test_account@test.com');
+        });
+    });
+
+}
