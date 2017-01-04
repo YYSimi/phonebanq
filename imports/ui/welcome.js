5572b9d624125e952e9b23899328f8225e4ff109
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2017-01-04 01:54:42 -0500

    adds awesome fade-in/fade-out animation

--- imports/ui/welcome.js
+++ imports/ui/welcome.js
@@ -1,1 +1,13 @@
 import './welcome.html';
+
+Template.welcome.onRendered(() => {
+    Tracker.autorun(() => {
+        if (!!Session.get("congresspeople")) {
+            console.log("fading");
+            console.log($('#js-featurettes'));
+            $('#js-featurettes').fadeOut(1000, () => {
+                $('#js-anon-tasks').fadeIn(1000);
+            });
+        }
+    })
+})
