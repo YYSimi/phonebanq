1ac5dfc94dcc5938248a7c229a46ac89080b9258
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2017-01-05 23:33:54 -0500

    moved script loading away from the main template and into startup code

--- client/main.js
+++ client/main.js
@@ -2,3 +2,14 @@ import '../imports/startup/index.js'
 import '../imports/ui/body.js';
 
 Meteor.subscribe('userTasks');
+
+Meteor.startup(function() {
+    // Jquery-validation
+    $.getScript('https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js');
+
+    // Select2
+    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js");
+
+    // QuillJS
+    $.getScript("https://cdn.quilljs.com/1.1.8/quill.min.js");
+})
