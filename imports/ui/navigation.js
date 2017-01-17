0d2b578e2480774c584261f367ee3a1f2d09f900
Youlian Simidjiyski <ysimidjiyski@gmail.com>
2017-01-16 23:56:03 -0500

    Hacks around duplicate loginbuttons template issue

--- imports/ui/navigation.js
+++ imports/ui/navigation.js
@@ -18,5 +18,29 @@ Template.nav.helpers({
     },
     fIsThisRouteActive(routeName) {
         return Router.current().route.getName() === routeName;
+    },
+    // HACK:  Can only have one instance of loginbuttons on a page due to meteor bug.  
+    // Need to un-render Loginbuttons Template, rather than just hiding it.
+    fIsLeftAlignedLoginbutonsVisible() {
+        return Template.instance().fIsLeftAlignedLoginbutonsVisible.get();
     }
+})
+
+
+// HACK:  Can only have one instance of loginbuttons on a page due to meteor bug.  
+// Need to track visibility state and un-render Loginbuttons Template on hide, rather than just hiding it.
+Template.nav.onCreated( () => {
+    var tmpl = Template.instance(); 
+    tmpl.fIsLeftAlignedLoginbutonsVisible = new ReactiveVar("false");
+
+    $(window).resize(
+        _.debounce(function(){
+            tmpl.fIsLeftAlignedLoginbutonsVisible.set( $('.left-aligned-loginbuttons').is(':visible'));
+        }, 300, false)
+    );
+})
+
+Template.nav.onRendered(() => {
+    var tmpl = Template.instance();
+    tmpl.fIsLeftAlignedLoginbutonsVisible.set( $('.left-aligned-loginbuttons').is(':visible'));
 })
